940fde041cfa511b7ad562b183630bcfd8ac3322
DESKTOP-UAN9SV9\sam31191 <shashankbalodi649@gmail.com>
2017-12-04 21:14:38 +0530

    new file added

---
+++ demo.php
@@ -0,0 +1,8 @@
+<?php
+   $var = 5;
+   $vars = $var + '10this is a string';
+   //echo $vars;
+   //echo (is_nan($vars)) ? 'not a number' : $vars; 
+   //echo gettype($vars); 
+echo date('s');   
+?>
